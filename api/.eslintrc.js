3ee39fa193ecfbe85a456908a72f424f1c5fd791
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-26 17:08:29 -0300

    api: linting config

---
+++ api/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base', 'prettier',
+  ],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 0,
+  },
+};
