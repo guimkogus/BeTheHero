2a83a44b630e0b7956d0f3b4bb1357d5d946843a
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-26 19:08:23 -0300

    api: incidents controller

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 
 import OngController from './controllers/OngController';
+import IncidentController from './controllers/IncidentController';
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 export default routes;
