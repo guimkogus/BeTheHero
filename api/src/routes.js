5e62d6c6f1ad2d5440d9c5d66c83b63fc061655a
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-26 18:28:45 -0300

    api: ongs list

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,24 +1,10 @@
 import express from 'express';
-import crypto from 'crypto';
 
-import connection from './database/connection';
+import OngController from './controllers/OngController';
 
 const routes = express.Router();
 
-routes.post('/ongs', async (req, res) => {
-  const { name, email, whatsapp, city, uf } = req.body;
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  });
-
-  return res.json({ id });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
 
 export default routes;
