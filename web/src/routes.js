5fadf680442769f3bc283590392d9a27b9e785d5
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-27 17:45:37 -0300

    web: NewIncident page

--- web/src/routes.js
+++ web/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
   return (
@@ -14,6 +15,8 @@ export default function Routes() {
         <Route path="/register" component={Register} />
 
         <Route path="/profile" component={Profile} />
+
+        <Route path="/incidents/new" component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
