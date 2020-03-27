a105057a897a73dd8ac28fee65d65f59cdac0ab0
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-27 15:04:17 -0300

    web: register page setup

--- web/src/routes.js
+++ web/src/routes.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
+import Register from './pages/Register';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
+
+        <Route path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   );
