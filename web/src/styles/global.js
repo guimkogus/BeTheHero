fc2fe8d493be44cd208d861eb531ff8c76a74b2e
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-27 14:00:47 -0300

    web: main page setup

--- web/src/styles/global.js
+++ web/src/styles/global.js
@@ -1,24 +1,29 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap');
+
   * {
     margin: 0;
     padding: 0;
     outline: 0;
     box-sizing: border-box;
   }
+
   html, body, #root {
     min-height: 100%;
   }
+
   body {
-    background: #7159c1;
+    background: #f0f0f5;
+    font: 400 14px Roboto, sans-serif;
     -webkit-font-smoothing: antialiased !important;
   }
-  body, input, button {
-    color: #222;
-    font-size: 14px;
-    font-family: Arial, Helvetica, sans-serif;
+
+  input, button, textarea {
+    font: 400 18px Roboto, sans-serif;
   }
+
   button {
     cursor: pointer;
   }
