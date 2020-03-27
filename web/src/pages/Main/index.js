419f6e593bf5a95316ef709080583c317e6fed30
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-27 14:29:45 -0300

    web: main styles

--- web/src/pages/Main/index.js
+++ web/src/pages/Main/index.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
 
-// import { Title } from './styles';
+import { Container, Section } from './styles';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Main() {
   return (
-    <div className="logon-container">
-      <section className="form">
-        <img src={logoImg} alt="Be The Hero" />
+    <Container>
+      <div className="logon-container">
+        <Section>
+          <section className="form">
+            <img src={logoImg} alt="Be The Hero" />
+            <form>
+              <h1>Faça seu logon</h1>
 
-        <form>
-          <h1>Faça seu logon</h1>
+              <input placeholder="Sua ID" />
+              <button className="button" type="submit">
+                Entrar
+              </button>
 
-          <input placeholder="Sua ID" />
-          <button type="submit">Entrar</button>
-
-          <a href="/register">
-            <FiLogIn size={16} color="#E02041" />
-            Não tenho cadastro
-          </a>
-        </form>
-      </section>
-      <img src={heroesImg} alt="Heroes" />
-    </div>
+              <a href="/register">
+                <FiLogIn size={16} color="#E02041" />
+                Não tenho cadastro
+              </a>
+            </form>
+          </section>
+        </Section>
+        <img src={heroesImg} alt="Heroes" />
+      </div>
+    </Container>
   );
 }
--- web/src/pages/Main/styles.js
+++ web/src/pages/Main/styles.js
@@ -1,12 +1,48 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 24px;
-  color: #fff;
-  font-family: Arial, Helvetica, sans-serif;
+export const Container = styled.div`
+  .logon-container {
+    width: 100%;
+    max-width: 1120px;
+    height: 100vh;
+    margin: 0 auto;
 
-  p {
-    font-size: 14px;
-    color: #333;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const Section = styled.form`
+  section {
+    width: 100%;
+    max-width: 350px;
+    margin-right: 50px;
+
+    form {
+    margin-top: 100px;
+  }
+
+  h1 {
+    font-size: 32px;
+    margin-bottom: 32px;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    margin-top: 40px;
+    color: #41414d;
+    font-size: 18px;
+    text-decoration: none;
+    font-weight: 500;
+    transition: opacity 0.2s;
+  }
+  a:hover {
+    opacity: 0.8;
+  }
+
+  a svg {
+    margin-right: 8px;
   }
 `;
