6e438c8c9c19023e7205f205ae3787d7899e0c82
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-27 13:22:29 -0300

    web: styled-comp setup

---
+++ web/src/pages/Main/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 24px;
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+
+  p {
+    font-size: 14px;
+    color: #333;
+  }
+`;
