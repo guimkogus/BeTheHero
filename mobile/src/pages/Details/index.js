daf6bb88b5310b229e3031d518de4c0f04d29b44
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-28 18:42:49 -0300

    mobile: details page

--- mobile/src/pages/Details/index.js
+++ mobile/src/pages/Details/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 export default function Details() {
-  return(
+  const navigation = useNavigation();
+
+  function navigateGoBack() {
+    navigation.goBack();
+  }
+
+  return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImg} />
-        <Text style={styles.headerText}>
-          Details page
-        </Text>
+
+        <TouchableOpacity onPress={navigateGoBack}>
+          <Feather name="arrow-left" size={27} color="#e02041" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+        <Text style={styles.incidentValue}>APAE</Text>
+
+        <Text style={styles.incidentProperty}>CASO:</Text>
+        <Text style={styles.incidentValue}>Acidente</Text>
+
+        <Text style={styles.incidentProperty}>VALOR:</Text>
+        <Text style={styles.incidentValue}>R$ 300,00</Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.contactTitle}>Salve o dia!</Text>
+        <Text style={styles.contactTitle}>Seja o herói deste caso.</Text>
+        <Text style={styles.contactDescription}>Entre em contato:</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>Whatsapp</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>E-Mail</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
