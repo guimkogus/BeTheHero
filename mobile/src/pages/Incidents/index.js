dd9cf9a3cb56c1306579562a6c7db69d93d42fa9
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-28 19:32:31 -0300

    mobile: infinite scroll

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -11,6 +11,9 @@ export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
 
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
   const navigation = useNavigation();
 
   function navigateToDetail(incident) {
@@ -18,10 +21,24 @@ export default function Incidents() {
   }
 
   async function loadIncidents() {
-    const res = await api.get('/incidents');
+    if (loading) {
+      return;
+    }
 
-    setIncidents(res.data);
+    if (total > 0 && incidents.length == total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const res = await api.get('incidents', {
+      params: { page }
+    });
+
+    setIncidents([...incidents, ...res.data]);
     setTotal(res.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -44,6 +61,8 @@ export default function Incidents() {
         data={incidents}
         keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
