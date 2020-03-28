01a5eb0420806f29acb8d062b6a9bd8f4567d1c1
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-03-28 18:10:07 -0300

    mobile: static Incidents page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,18 +1,52 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 export default function Incidents() {
-  return(
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate('Details');
+  }
+
+  return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImg} />
         <Text style={styles.headerText}>
-          Incidents page
+          Total de <Text style={styles.headerTextBold}>0 casos</Text>.
         </Text>
       </View>
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>Escolha um dos casos abaixo e seja o heroi!</Text>
+
+      <FlatList
+        style={styles.incidentsList}
+        data={[1, 2, 3, 4, 5]}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APAE</Text>
+
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>Acidente</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>R$ 300,00</Text>
+
+            <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
